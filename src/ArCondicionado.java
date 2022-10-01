94fdc644b9fd9f45bce5874f58ec1ef7495b1e1a
DarkSan-cloud <evancavaraujo3@gmail.com>
2022-10-01 17:17:43 -0300

    Implementação de métodos e melhorias no código,.

--- src/ArCondicionado.java
+++ src/ArCondicionado.java
@@ -1,35 +1,114 @@
 public class ArCondicionado {
     private boolean estaligado;
     private String status;
-    private int temperatura= 23, min = 16, max = 30;
+    private int temperatura = 23, min = 16, max = 30;
 
     private String modooperacao;
     private String marca;
     private String modelo;
     private String capacidade;
 
-    public void ligar() {
-        estaligado = true;
+    public ArCondicionado() {
+        marca = "ELGIN";
+        modelo = "SPLIT INVERTER";
+        capacidade = "12.000 BTUS";
+        modooperacao = "r";
+    }
+    // METODOS
+
+    // Ligar de desligar
+    public void liga_desliga() {
+        if (estaligado) {
+            estaligado = false;
+        } else {
+            estaligado = true;
+            System.out.println("Ar Condicionado ligado!");
+        }
     }
 
-    public void desligar() {
-        estaligado = false;
+    // MUDAR MODO DE OPERAÇÃO
+    public void modoop() {
+        if (estaligado) {
+            if (modooperacao == "r") {
+                modooperacao = "v";
+                System.out.println("Modo Ventilação Ativado!");
+
+            } else if (modooperacao == "v") {
+                modooperacao = "d";
+                System.out.println("Modo Desumidificação Ativado!");
+
+            } else if (modooperacao == "d") {
+                modooperacao = "a";
+                System.out.println("Modo Aquecer Ativado!");
+
+            } else {
+                modooperacao = "r";
+                System.out.println("Modo de Refrigeração Ativado!");
+            }
+        }
     }
 
+    // aumentarTemperatura
     public void aumentarTemperatura() {
-        if (temperatura == max) {
-            return;
+        if (estaligado) {
+            if (modooperacao == "r" || modooperacao == "a") {
+                if (temperatura < max) {
+                    temperatura++;
+                    System.out.println("Temperatura aumentada para " + temperatura + "ºC");
+                }
+            }
         }
-        temperatura += 1;
     }
 
+    // diminuirTemperatura
     public void diminuirTemperatura() {
-        if (temperatura == min) {
-            return;
+        if (estaligado) {
+            if (modooperacao == "r" || modooperacao == "a") {
+                if (temperatura > min) {
+                    temperatura--;
+                    System.out.println("Temperatura diminuída para " + temperatura + "ºC");
+                }
+            }
         }
-        temperatura -= 1;
     }
 
+    // STATUS
+    public void exibirStatus() {
+        if (!estaligado) {
+            System.out.println("Ar condicionado desligado");
+        } else {
+            System.out.println("Marca.:" + marca);
+            System.out.println("Modelo.:" + modelo);
+            System.out.println("Capacidade." + capacidade);
+            if (modooperacao == "r") {
+                System.out.println("Modo.: Refrigeração");
+                System.out.println("Temperatura.:" + temperatura + "C");
+            } else {
+                System.out.println("Modo.: Ventilação");
+            }
+        }
+    }
+
+    // DESUMIDIFICAÇÃO
+    public void desumidificar() {
+        if (estaligado) {
+            if (modooperacao == "d") {
+                temperatura = 23;
+
+            }
+        }
+    }
+
+    // Aquecer
+    public void aquecer() {
+        if (estaligado) {
+            if (modooperacao == "a") {
+                System.out.println("Modo aquecer ativado");
+            }
+        }
+    }
+
+    // GET AND SET
     public boolean isEstaligado() {
         return estaligado;
     }
--- src/Controle.java
+++ src/Controle.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Controle {
     public static void main(String[] args) {
-        
+
+        Scanner leia = new Scanner(System.in);
+        ArCondicionado AC = new ArCondicionado();
+        int op;
+        do {
+            System.out.println("1 - Ligar/Desligar");
+            System.out.println("2 - Mudar modo de operação");
+            System.out.println("3 - Aumentar temperatura");
+            System.out.println("4 - Diminuir Temperatura");
+            System.out.println("5 - Status");
+            System.out.println("6 - Sair");
+            op = leia.nextInt();
+
+            switch (op) {
+                case 1:
+                    AC.liga_desliga();
+                    break;
+                    
+                case 2:
+                    AC.modoop();
+                    break;
+
+                case 3:
+                    AC.aumentarTemperatura();
+                    break;
+
+                case 4:
+                    AC.diminuirTemperatura();
+                    break;
+
+                case 5:
+                    AC.exibirStatus();
+                    break;
+
+                case 6:
+                    System.exit(0);
+                    break;
+                    
+                default:
+                    System.out.println("Opção invalida");
+            }
+
+        } while (op != 6);
     }
 }
